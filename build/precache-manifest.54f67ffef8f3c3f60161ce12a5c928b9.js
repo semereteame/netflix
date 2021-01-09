5891431375786a99238c1262d072049786529a8f
Semere Ghebremariam <semateame@Semeres-MacBook-Pro.local>
2021-01-08 21:28:56 -0600

    last-commit

---
+++ build/precache-manifest.54f67ffef8f3c3f60161ce12a5c928b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3210c8163302c3eea6fca4fed19510e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77533cdda45f896d918b",
+    "url": "/static/css/main.7f4f1558.chunk.css"
+  },
+  {
+    "revision": "755449dc6e6c3ee071d0",
+    "url": "/static/js/2.7009a159.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7009a159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77533cdda45f896d918b",
+    "url": "/static/js/main.88426396.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
